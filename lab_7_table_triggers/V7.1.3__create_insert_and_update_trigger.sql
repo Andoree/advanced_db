85f10f3a1cc3f6119e5c76172bb6fbd42e583df0
Andoree <and2900@inbox.ru>
2020-04-18 15:34:06 +0300

    Table triggers lab. Added update trigger

--- lab_7_table_triggers/V7.1.3__create_insert_and_update_trigger.sql
+++ lab_7_table_triggers/V7.1.3__create_insert_and_update_trigger.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION redirect_rows_to_childs()
+CREATE OR REPLACE FUNCTION redirect_inserts_to_childs()
 RETURNS TRIGGER AS
 $BODY$
 DECLARE
@@ -12,7 +12,7 @@ IF NOT EXISTS
 THEN
 EXECUTE format(E'CREATE TABLE %I () INHERITS (public.hub_table_triggers_lab)', partition_name );
 EXECUTE format('ALTER TABLE %I ADD CONSTRAINT partition_check
-	CHECK(id >= %s * 10000 and id < (%s + 1) * 10000)', partition_name, new_child_id, new_child_id);
+	CHECK(id >= %s * 10000 AND id <= (%s + 1) * 10000) ', partition_name, new_child_id, new_child_id);
 END IF;
 EXECUTE format('INSERT INTO %I VALUES($1,$2)', partition_name) using NEW.id, NEW.text_column;
 
@@ -22,12 +22,45 @@ END;
 $BODY$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER redirect_traffic_to_childs
+CREATE TRIGGER redirect_insert_traffic_to_childs
 BEFORE INSERT ON
 	public.hub_table_triggers_lab
 	FOR EACH ROW
-EXECUTE FUNCTION redirect_rows_to_childs();
+EXECUTE FUNCTION redirect_inserts_to_childs();
 
 
 
+CREATE OR REPLACE FUNCTION manage_child_updates()
+RETURNS TRIGGER AS
+$BODY$
+DECLARE
+old_child_id INTEGER := DIV(OLD.id, 10000);
+new_child_id INTEGER := DIV(NEW.id, 10000);
+old_partition_name VARCHAR := 'inherited_child_with_trigger_' || old_child_id;
+new_partition_name VARCHAR := 'inherited_child_with_trigger_' || new_child_id;
+BEGIN
+
+IF NOT EXISTS
+	(SELECT 1 FROM information_schema.tables
+		WHERE  table_name = new_partition_name) 
+THEN
+EXECUTE format(E'CREATE TABLE %I () INHERITS (public.hub_table_triggers_lab)', partition_name);
+EXECUTE format('ALTER TABLE %I ADD CONSTRAINT partition_check
+	CHECK(id >= %s * 10000 AND id <= (%s + 1) * 10000)', partition_name, new_child_id, new_child_id);
+END IF;
+EXECUTE format('DELETE FROM %I WHERE id = %s', old_partition_name) using OLD.id;
+EXECUTE format('INSERT INTO %I VALUES($1,$2)', new_partition_name) using NEW.id, NEW.text_column;
+
+RETURN NULL;
+
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER redirect_update_traffic_to_childs
+BEFORE UPDATE OF id ON
+	public.hub_table_triggers_lab
+	FOR EACH ROW
+	WHEN(OLD.id IS DISTINCT FROM NEW.id)
+EXECUTE FUNCTION manage_child_updates();
 
