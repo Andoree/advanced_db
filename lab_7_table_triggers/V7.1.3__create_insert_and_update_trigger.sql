c7e584db08dc8a0c3391883201aa396215942b30
root <root@ip-172-30-5-82.ec2.internal>
2020-04-18 12:15:32 +0000

    Created insert trigger

--- lab_7_table_triggers/V7.1.3__create_insert_and_update_trigger.sql
+++ lab_7_table_triggers/V7.1.3__create_insert_and_update_trigger.sql
@@ -10,10 +10,10 @@ IF NOT EXISTS
 	(SELECT 1 FROM information_schema.tables
 		WHERE  table_name = partition_name) 
 THEN
-EXECUTE format(E'CREATE TABLE %I() INHERITS (public.hub_table_triggers_lab)', partition_name, );
-
+EXECUTE format(E'CREATE TABLE %I () INHERITS (public.hub_table_triggers_lab)', partition_name );
 EXECUTE format('ALTER TABLE %I ADD CONSTRAINT partition_check
 	CHECK(id >= %s * 10000 and id < (%s + 1) * 10000)', partition_name, new_child_id, new_child_id);
+END IF;
 EXECUTE format('INSERT INTO %I VALUES($1,$2)', partition_name) using NEW.id, NEW.text_column;
 
 RETURN NULL;
@@ -26,7 +26,7 @@ CREATE TRIGGER redirect_traffic_to_childs
 BEFORE INSERT ON
 	public.hub_table_triggers_lab
 	FOR EACH ROW
-EXECUTE redirect_rows_to_childs();
+EXECUTE FUNCTION redirect_rows_to_childs();
 
 
 
