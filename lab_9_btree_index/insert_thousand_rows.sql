7648a243ecaa0ad056230312c0c924686bfcfa6d
root <root@ip-172-30-5-217.ec2.internal>
2020-04-24 20:36:23 +0000

    B-tree lab. Fixed errors in insert script and select from pg_stat_user_indexes

--- lab_9_btree_index/insert_thousand_rows.sql
+++ lab_9_btree_index/insert_thousand_rows.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.index_lab_1_table
 	SELECT (random() * 10)::INTEGER,
-	SELECT (random() * 10)::INTEGER,
-	SELECT (random() * 10)::INTEGER,
+	(random() * 10)::INTEGER,
+	(random() * 10)::INTEGER
 FROM generate_series(1, 1000) as k(i);
--- lab_9_btree_index/select_from_pg_stat_user_indexes.sql
+++ lab_9_btree_index/select_from_pg_stat_user_indexes.sql
@@ -1,10 +1,9 @@
-SELECT relid::regclass AS public.index_lab_1_table,
+SELECT relid::regclass AS table_name,
 indexrelid::regclass AS index_name,
 idx_tup_read, 
 idx_tup_fetch, 
 idx_scan
 FROM pg_stat_user_indexes JOIN pg_index USING(indexrelid)
-WHERE idx_scan > 0
-AND NOT indisunique;
+WHERE idx_scan > 0 AND NOT indisunique;
 
 
