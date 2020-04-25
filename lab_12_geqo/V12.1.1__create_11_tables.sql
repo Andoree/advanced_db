953f5bb1a1da50e78bfb01b52a0f9d61a2277877
Andoree <and2900@inbox.ru>
2020-04-25 13:51:51 +0300

    Geqo lab flyway script to create tables, benchmark with 10 joins

---
+++ lab_12_geqo/V12.1.1__create_11_tables.sql
@@ -0,0 +1,46 @@
+CREATE TABLE public.geqo_t1(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t2(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t3(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t4(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t5(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t6(
+	id INTEGER,
+	name VARCHAR
+);
+
+CREATE TABLE public.geqo_t7(
+	id INTEGER,
+	name VARCHAR
+);
+
+CREATE TABLE public.geqo_t8(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t9(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t10(
+	id INTEGER,
+	name VARCHAR
+);
+CREATE TABLE public.geqo_t11(
+	id INTEGER,
+	name VARCHAR
+);
