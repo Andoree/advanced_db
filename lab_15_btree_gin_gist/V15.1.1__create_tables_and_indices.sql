ac80e1983c68009d1d29da1f231531c106fb41c9
root <root@ip-172-30-5-217.ec2.internal>
2020-05-04 12:28:26 +0000

    Indices on text lab. Change table and indices creation script

--- lab_15_btree_gin_gist/V15.1.1__create_tables_and_indices.sql
+++ lab_15_btree_gin_gist/V15.1.1__create_tables_and_indices.sql
@@ -15,6 +15,6 @@ CREATE TABLE public.lab_indices_on_text_gist(
 CREATE EXTENSION pg_trgm;
 
 
-CREATE INDEX b_tree_index_on_text_lab ON public.lab_indices_on_text_btree(content gin_trgm_ops);
+CREATE INDEX b_tree_index_on_text_lab ON public.lab_indices_on_text_btree(content);
 CREATE INDEX gin_index_on_text_lab ON public.lab_indices_on_text_gin USING GIN(content gin_trgm_ops);
 CREATE INDEX gist_index_on_text_lab ON public.lab_indices_on_text_gist USING GIST(content gist_trgm_ops);
