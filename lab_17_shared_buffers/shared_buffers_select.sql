6f8a7e4fbd005ff86dbd60f4ad23b6a079042a75
root <root@ip-172-30-5-217.ec2.internal>
2020-05-23 13:15:09 +0000

    Fixed select

--- lab_17_shared_buffers/shared_buffers_select.sql
+++ lab_17_shared_buffers/shared_buffers_select.sql
@@ -1,14 +1,15 @@
 SELECT c.relname,
 d.datname,
-pg_size_pretty(count(*) * current_settings('block_size')::INTEGER) AS buffered,
+pg_size_pretty(count(*) * current_setting('block_size')::INTEGER) AS buffered,
 round(100.0 * count(*)/
 (SELECT setting FROM pg_settings
 WHERE name='shared_buffers')::INTEGER, 1)
 AS buffer_percent,
 round(100.0 * count(*) * current_setting ('block_size')::INTEGER/ pg_table_size(c.oid), 1) AS percent_of_relation
 FROM pg_class c
-	INNER JOIN pg_buffercache b
-		ON b.relfilenode = pg_relation_filenode(c.oid)
-	INNER JOIN pg_database d ON b.reldatabase = d.oid
-GROUP BY c.oid, c.relname, d.datname;
-
+        INNER JOIN pg_buffercache b
+                ON b.relfilenode = pg_relation_filenode(c.oid)
+        INNER JOIN pg_database d ON b.reldatabase = d.oid
+WHERE relname LIKE 'shared_buffers_lab%' 
+GROUP BY c.oid, c.relname, d.datname
+ORDER BY percent_of_relation DESC;
